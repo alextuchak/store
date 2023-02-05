92d8ca5cc87da0f9689233d6df001677b2c15c75
alextuchak <alextuchak@gmail.com>
2023-02-05 23:07:32 +0500

    Users login and registration forms added, templates for forms added

--- store_stepik/urls.py
+++ store_stepik/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from products.views import index, products
+from products.views import index
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include('products.urls', namespace="products"))
+    path('products/', include('products.urls', namespace="products")),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:

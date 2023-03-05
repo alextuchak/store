a03562a293dac06be7fb89f66997222371798e68
alextuchak <alextuchak@gmail.com>
2023-03-05 11:08:03 +0500

    email verification send funcion added. email_verification template for expiried link added

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
-from django.urls import path, include
+from django.urls import path
 
 from users.views import (EmailVerificationView, UserLoginView, UserProfileView,
-                         UserRegistrationView)
+                         UserRegistrationView, send_verification_email)
 
 app_name = 'users'
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name="email_verification"),
+    path('verify/send_email/', send_verification_email, name='send_verification'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,18 +1,23 @@
 from django.contrib.auth.views import LoginView, TemplateView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
 from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
-
+import uuid
+from datetime import timedelta
+from django.utils.timezone import now
 from common.views import TitleMixin
 from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
 from users.models import EmailVerification, User
+from store_stepik.settings import DOMAIN_NAME, EMAIL_HOST_USER
 
 
 class UserLoginView(TitleMixin, LoginView):
     template_name = 'users/login.html'
     form_class = UserLoginForm
-    title = 'Store - Авторищация'
+    title = 'Store - Авторизация'
 
 
 class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
@@ -53,6 +58,32 @@ class EmailVerificationView(TitleMixin, TemplateView):
         if email_verifications.exists() and not email_verifications.first().is_expired():
             user.is_verified_email = True
             user.save()
-            return super(EmailVerificationView, self).get(request, *args, **kwargs)
-        else:
-            return HttpResponseRedirect(reverse('index'))
+        return super(EmailVerificationView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(EmailVerificationView, self).get_context_data()
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verifications = EmailVerification.objects.filter(user=user, code=code)
+        if email_verifications.exists() and email_verifications.first().is_expired():
+            context['expired'] = True
+        return context
+
+
+@login_required
+def send_verification_email(request, *args, **kwargs):
+    user = request.user
+    record = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=now() + timedelta(days=2))
+    verification_link = f"{DOMAIN_NAME}" \
+                        f"{reverse('users:email_verification', kwargs={'email': user.email, 'code': record.code})}"
+    subject = f"Подтверждение учетной записи для {user.username}"
+    message = "Для подтверждения учетной записи для {} перейдите по ссылке: {}".format(
+        user.email, verification_link)
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=EMAIL_HOST_USER,
+        recipient_list=[user.email],
+        fail_silently=False
+    )
+    return HttpResponseRedirect(reverse('index'))

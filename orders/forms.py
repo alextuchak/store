5d720a41b7c07cb686f88992c3608ee51fa7ed8c
alextuchak <alextuchak@gmail.com>
2023-03-28 09:40:19 +0500

    orders view and form changes

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,14 @@ from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иван'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иванов'}))
+    cell_phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                               'placeholder': '+7 XXX-XXX-XX-XX'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Россия, Москва, ул. Ленина, дом 1, кв. 1'
+    }))
+
     class Meta:
         model = Order
-        fields = ('first_name', 'last_name', 'email', 'address')
+        fields = ('first_name', 'last_name', 'cell_phone', 'address')

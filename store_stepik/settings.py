79ad8ea9a355748143e6354cafbe0805d31f6c65
alextuchak <alextuchak@gmail.com>
2023-10-10 03:53:49 +0500

    base dir path changed

--- store_stepik/settings.py
+++ store_stepik/settings.py
@@ -18,7 +18,7 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
